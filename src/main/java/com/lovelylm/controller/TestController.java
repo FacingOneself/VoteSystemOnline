4763c38c407164d840747ae2ad032ab3884a70d2
Story <44112>
2019-06-18 08:53:25 +0800

    56

--- src/main/java/com/lovelylm/controller/TestController.java
+++ src/main/java/com/lovelylm/controller/TestController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
     @RequestMapping("hello")
     public String hello(){
-        return "hello";
+        return "5954";
     }
 }
